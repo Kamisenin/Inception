e7398b5ba544a044770e24fa338373f881dc53fb
Kamisenin <senelle.camille18@gmail.com>
2025-11-25 16:29:24 +0100

    Clenup of database and fixed Root pswd change bug

--- srcs/database/setup_db.sh
+++ srcs/database/setup_db.sh
@@ -4,35 +4,35 @@ set -e
 
 echo "Generating MariaDB configuration..."
 
+echo "$MYSQL_ROOT_PASSWORD and $WORDPRESS_DB and $WORDPRESS_USER and $WORDPRESS_PASSWORD"
 
-    echo "Modifying root password..."
-    SQL_FILE="/tmp/root.sql"
-    cat > "$SQL_FILE" << SQL
+echo "Starting MariaDB in skip-grant-tables mode..."
+mysqld_safe --skip-grant-tables --user=root &
+echo "Waiting for MariaDB to start..."
+while ! mysqladmin ping --silent 2>/dev/null; do
+    sleep 1
+done
+
+echo "Modifying root password..."
+mysql -u mysql << SQL
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
-EXIT
 SQL
 
-    mysqld_safe --skip-grant-tables --user=root < $SQL_FILE
+echo "Initializing Wordpress Database..."
 
-    rm -f "$SQL_FILE"
-
-    echo "Initializing Wordpress Database..."
-
-    SQL_FILE="/tmp/init.sql"
-    cat > "$SQL_FILE" << SQL
-CREATE DATABASE IF NOT EXISTS `${WORDPRESS_DB}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+mysql -u root -p"$MYSQL_ROOT_PASSWORD"  << SQL
+CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS '${WORDPRESS_USER}'@'%' IDENTIFIED BY '${WORDPRESS_PASSWORD}';
-GRANT ALL PRIVILEGES ON `${WORDPRESS_DB}`.* TO '${WORDPRESS_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${WORDPRESS_DB}\`.* TO '${WORDPRESS_USER}'@'%';
 FLUSH PRIVILEGES;
-EXIT
 SQL
 
-    mariadbd < "$SQL_FILE"
-    rm -f "$SQL_FILE"
+echo "Stopping MariaDB..."
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-    echo "Initialization done..."
+echo "Initialization done..."
 
-echo "Starting MariaDB..."
+echo "Starting MariaDB normally..."
 exec "$@"
